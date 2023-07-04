b5f057e81bb930240d21d1c1362ab69ee97c70f2
ras Bamba <b24media.info@gmail.com>
2023-07-04 14:41:32 -0700

    Update 1.0

---
+++ database/client/src/components/MainPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+const MainPage = () => {
+    return ( 
+        <div className="main-page">
+            <h1>Welcome to my MERN Website</h1>
+
+
+        </div>
+     );
+};
+ 
+export default MainPage;
---
+++ database/client/src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+const Sidebar = () => {
+    return ( 
+        <div className='sidebar'>
+            <h2>Navigation</h2>
+            <ul>
+                <li>Home</li>
+                <li>Home2</li>
+                <li>Home3</li>
+            </ul>
+
+        </div>
+     );
+}
+ 
+export default Sidebar ;
